798798e8154b956a37f55234fe382f00acec6d43
Jan Pecinovsky <janpecinovsky@gmail.com>
2015-03-13 12:01:53 +0100

    Simple dice, absolute spacing etc.

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,9 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, TextAreaField, DecimalField, SelectField, DateField, BooleanField
+from wtforms import IntegerField
 from wtforms.validators import DataRequired as Required
 from wtforms.validators import Length, optional,Regexp,NumberRange
 from flask import g
+
+class dobbel_form(Form):
+	lower = IntegerField('lower',validators=[Required()])
+	upper = IntegerField('upper',validators=[Required()])
--- app/views.py
+++ app/views.py
@@ -1,12 +1,12 @@
 from app import app, lm, facebook, db
 from flask.ext.login import current_user, login_required, login_user, logout_user
 from flask import g, render_template, redirect, flash, session, url_for, request, abort, send_from_directory, send_file
-from models import User, Category, Entry, File
-from forms import category_form, delete_form, entry_form
+from forms import *
 from datetime import datetime
 from werkzeug import secure_filename
 from config import ALLOWED_EXTENSIONS, UPLOAD_FOLDER, FILE_FOLDER
 import os
+import random
 
 @app.route('/favicon.ico')
 def favicon():
@@ -28,8 +28,19 @@ def internal_error(error):
     flash('Something went wrong, sorry!')
     return redirect(url_for('index'))
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods = ['GET','POST'])
+@app.route('/index', methods = ['GET','POST'])
 def index():
-	return render_template(
-		'index.html')
+    form = dobbel_form()
+    if form.validate_on_submit():
+        lower = form.lower.data
+        upper = form.upper.data
+        number = int(random.random()*(upper-(lower-1)) + lower)
+    else:
+        number = None
+        form.lower.data = 1
+        form.upper.data = 6
+    return render_template(
+		'index.html',
+        form=form,
+        number=number)
